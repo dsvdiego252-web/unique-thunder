8045ef22ec4f69992b163f63b0595bd8f49d0d1a
dsvdiego252-web <dsvdiego252@gmail.com>
2025-11-07 14:06:57 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // CONFIG DA LOJA
-const PAYMENT_LINK = "http://link.mercadopago.com.br/uniquethunder"; // cole aqui o link de pagamento do Mercado Pago (Checkout Pro ou Link de Pagamento)
+const PAYMENT_LINK = "https://link.mercadopago.com.br/uniquethunder"; // cole aqui o link de pagamento do Mercado Pago (Checkout Pro ou Link de Pagamento)
 const SHIP_TABLE = [
   { cep_start: "01000-000", cep_end: "19999-999", price: 24.90, free_over: 299.00 },
   { cep_start: "20000-000", cep_end: "28999-999", price: 29.90, free_over: 349.00 },
